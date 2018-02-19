81aef38bf9c1aba6d8cfe483a4100e74a84ef620
Cas Burggraaf <casburggraaf@gmail.com>
2018-02-19 13:30:50 +0100

    Reduse api calls

--- app/static/app.js
+++ app/static/app.js
@@ -19,14 +19,22 @@
         },
         popular: function() {
           sections.toggle("popular", "popular");
-          // Fills the pages with data from the API except on the startpage
-          sections.loadPage("popular", "popular")
+          // Fills the pages with data from the API
+          if(Object.getOwnPropertyNames(xhr.dataPupular).length === 0) {
+            sections.loadPage("popular", "popular")
+          } else {
+            sections.renderPage("popular", "popular")
+          }
         },
         // Get movie IDs and add it as second parameter in section.toggle
         "movie/:movieId": function (movieId) {
           sections.toggle("movieDetail", movieId);
-          // Fills the pages with data from the API except on the startpage
-          sections.loadPage("movieDetail", movieId);
+          // Fills the pages with data from the API
+          if(!xhr.dataDetail[movieId]){
+            sections.loadPage("movieDetail", movieId);
+          } else {
+            sections.renderPage("movieDetail", movieId);
+          }
         }
       });
     }
@@ -74,9 +82,9 @@
         var target = this.sectionsElements[1].querySelector('#popularMovies');
         // Render Page (and check if filterd option is check if so show filterd data)
         if (xhr.filterBadMoviesCheck) {
-          Transparency.render(target, xhr.dataFilterd.results, directives);
+          Transparency.render(target, xhr.dataPupularFilterd.results, directives);
         } else {
-          Transparency.render(target, xhr.data.results, directives);
+          Transparency.render(target, xhr.dataPupular.results, directives);
         }
 
       } else if (route === "movieDetail"){
@@ -103,7 +111,7 @@
 
         var target = this.sectionsElements[2].querySelector('#movieDetails');
         // Render Page
-        Transparency.render(target, xhr.data, directives);
+        Transparency.render(target, xhr.dataDetail[apiSearchParm], directives);
       }
     }
   };
@@ -111,8 +119,9 @@
   const xhr = {
     apiBasisUrl: "https://api.themoviedb.org/3/movie/",
     apiKey: "d9a167a57e748b4a804b41f0186b2339",
-    data: {},
-    dataFilterd: {},
+    dataPupular: {},
+    dataPupularFilterd: {},
+    dataDetail: {},
     filterBadMoviesCheck: false,
     request(apiSearchParm){
       var _this = this;
@@ -128,11 +137,15 @@
         request.onload = function() {
           if (request.status >= 200 && request.status < 400) {
             // Sets the JSON result in the data attribute
-            _this.data = JSON.parse(request.responseText);
+
             // Checking if it a popular Search or Movie detail page
             if (apiSearchParm === "popular") {
+              _this.dataPupular = JSON.parse(request.responseText);
               _this.filterBadMovies();
               _this.releaseDateConvert();
+            } else {
+              _this.dataDetail.temp = JSON.parse(request.responseText);
+              _this.dataDetail[_this.dataDetail.temp.id] = _this.dataDetail.temp;
             }
             resolve();
           }
@@ -144,14 +157,14 @@
     },
     filterBadMovies(){
       // filtering if vote average is smaller than 6.5
-      this.dataFilterd.results = this.data.results.filter(function(obj){
+      this.dataPupularFilterd.results = this.dataPupular.results.filter(function(obj){
         if (obj.vote_average > 6.5) {
           return true;
         }
       });
     },
     releaseDateConvert(){
-      this.data.results = this.data.results.map(function(obj) {
+      this.dataPupular.results = this.dataPupular.results.map(function(obj) {
         obj.release_date = utils.calcReleaseDate(obj.release_date);
         return obj;
       });
@@ -164,7 +177,7 @@
       date = new Date(date[0], parseInt(date[1] - 1), date[2]);
       var days = Math.floor((new Date() - date) / 86400000);
       return days;
-    },
+    }
   };
 
   // Start the Aplication
