8a40180966270c9b04dcb79f013b0794f2be7491
Cas Burggraaf <casburggraaf@gmail.com>
2018-02-19 12:25:07 +0100

    Code refactory

--- app/static/app.js
+++ app/static/app.js
@@ -19,11 +19,14 @@
         },
         popular: function() {
           sections.toggle("popular", "popular");
-
+          // Fills the pages with data from the API except on the startpage
+          sections.loadPage("popular", "popular")
         },
         // Get movie IDs and add it as second parameter in section.toggle
         "movie/:movieId": function (movieId) {
           sections.toggle("movieDetail", movieId);
+          // Fills the pages with data from the API except on the startpage
+          sections.loadPage("movieDetail", movieId);
         }
       });
     }
@@ -35,8 +38,8 @@
     init(){
       // A event listener on the button, refreshes page
       this.sectionsElements[1].querySelector('#popular input').addEventListener("change",function () {
-        xhr.filterBadMovies = this.checked;
-        sections.toggle("popular", "popular");
+        xhr.filterBadMoviesCheck = this.checked;
+        sections.renderPage("popular", "popular");
       });
     },
     toggle(route, routeId) {
@@ -47,72 +50,16 @@
           el.classList.add("active");
         }
       });
-
-      // Fills the pages with data from the API except on the startpage
-      if(route != "start"){
-        this.loadPage(route, routeId);
-      }
     },
     loadPage(route, routeId){
       // Makes an api request
       xhr.request(routeId).then(function(){
         // Renders the page after the api call resolves
-        xhr.render(route, routeId);
+        sections.renderPage(route, routeId);
       });
     },
-    calcReleaseDate(date) {
-      date = date.split("-");
-      date = new Date(date[2], date[1], date[0]);
-      var days = Math.floor((new Date() - date) / 86400000);
-      return days;
-    }
-  };
-
-  const xhr = {
-    apiBasisUrl: "https://api.themoviedb.org/3/movie/",
-    apiKey: "d9a167a57e748b4a804b41f0186b2339",
-    data: {},
-    filterBadMovies: false,
-    request(apiSearchParm){
-      var _this = this;
-      // Makes a promise for the xml request
-      var promise = new Promise(function(resolve, reject) {
-        var request = new XMLHttpRequest();
-
-        // Making the url and creating a GET request
-        var url = `${_this.apiBasisUrl}${apiSearchParm}?api_key=${_this.apiKey}`;
-
-        request.open('GET', url, true);
-
-        request.onload = function() {
-          if (request.status >= 200 && request.status < 400) {
-            // Sets the JSON result in the data attribute
-            _this.data = JSON.parse(request.responseText);
-            resolve();
-          }
-        };
-        request.send();
-      });
-
-      return promise;
-    },
-    render(route, apiSearchParm) {
-      if (route === "popular") {
-        // Checking if it a popular Search or Movie detail page
-        if (xhr.filterBadMovies) {
-          // filtering if vote average is smaller than 6.5
-          this.data.results = this.data.results.filter(function(obj){
-            if (obj.vote_average > 6.5) {
-              return true;
-            }
-          });
-        }
-
-        this.data.results = this.data.results.map(function(obj) {
-          obj.release_date = sections.calcReleaseDate(obj.release_date);
-          return obj;
-        });
-
+    renderPage(route, apiSearchParm) {
+      if (apiSearchParm === "popular") {
         var directives = {
           title: {
             href: function(params) {
@@ -124,9 +71,14 @@
           }
         };
 
-        var target = sections.sectionsElements[1].querySelector('#popularMovies');
-        // Render Page
-        Transparency.render(target, this.data.results, directives);
+        var target = this.sectionsElements[1].querySelector('#popularMovies');
+        // Render Page (and check if filterd option is check if so show filterd data)
+        if (xhr.filterBadMoviesCheck) {
+          Transparency.render(target, xhr.dataFilterd.results, directives);
+        } else {
+          Transparency.render(target, xhr.data.results, directives);
+        }
+
       } else if (route === "movieDetail"){
         var directives = {
           title: {
@@ -149,11 +101,70 @@
           }
         };
 
-        var target = sections.sectionsElements[2].querySelector('#movieDetails');
+        var target = this.sectionsElements[2].querySelector('#movieDetails');
         // Render Page
-        Transparency.render(target, this.data, directives);
+        Transparency.render(target, xhr.data, directives);
       }
     }
+  };
+
+  const xhr = {
+    apiBasisUrl: "https://api.themoviedb.org/3/movie/",
+    apiKey: "d9a167a57e748b4a804b41f0186b2339",
+    data: {},
+    dataFilterd: {},
+    filterBadMoviesCheck: false,
+    request(apiSearchParm){
+      var _this = this;
+      // Makes a promise for the xml request
+      var promise = new Promise(function(resolve, reject) {
+        var request = new XMLHttpRequest();
+
+        // Making the url and creating a GET request
+        var url = `${_this.apiBasisUrl}${apiSearchParm}?api_key=${_this.apiKey}`;
+
+        request.open('GET', url, true);
+
+        request.onload = function() {
+          if (request.status >= 200 && request.status < 400) {
+            // Sets the JSON result in the data attribute
+            _this.data = JSON.parse(request.responseText);
+            // Checking if it a popular Search or Movie detail page
+            if (apiSearchParm === "popular") {
+              _this.filterBadMovies();
+              _this.releaseDateConvert();
+            }
+            resolve();
+          }
+        };
+        request.send();
+      });
+
+      return promise;
+    },
+    filterBadMovies(){
+      // filtering if vote average is smaller than 6.5
+      this.dataFilterd.results = this.data.results.filter(function(obj){
+        if (obj.vote_average > 6.5) {
+          return true;
+        }
+      });
+    },
+    releaseDateConvert(){
+      this.data.results = this.data.results.map(function(obj) {
+        obj.release_date = utils.calcReleaseDate(obj.release_date);
+        return obj;
+      });
+    }
+  };
+
+  const utils = {
+    calcReleaseDate(date) {
+      date = date.split("-");
+      date = new Date(date[0], parseInt(date[1] - 1), date[2]);
+      var days = Math.floor((new Date() - date) / 86400000);
+      return days;
+    },
   };
 
   // Start the Aplication
